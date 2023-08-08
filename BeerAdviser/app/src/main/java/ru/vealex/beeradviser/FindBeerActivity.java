b85bc093095cab401cfa5ba4087e48fd4edddb6a
Vealex <mrvealex@gmail.com>
2023-08-08 17:05:24 +0700

    Created interface and set button first functionality

--- BeerAdviser/app/src/main/java/ru/vealex/beeradviser/FindBeerActivity.java
+++ BeerAdviser/app/src/main/java/ru/vealex/beeradviser/FindBeerActivity.java
@@ -4,11 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+
+import android.widget.Spinner;
+
+import android.widget.TextView;
+
 public class FindBeerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+
+    //Called when the user clicks the button
+    public void onClickFindBeer(View view) {
+        //Получить ссылку на TextView
+        TextView brands = (TextView) findViewById(R.id.brands);
+        //Получить ссылку на Spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+        //Получить вариант, выбранный в Spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+        //Вывести выбранный вариант
+        brands.setText(beerType);
     }
 }
