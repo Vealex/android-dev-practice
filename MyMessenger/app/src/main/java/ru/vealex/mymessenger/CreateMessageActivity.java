6827e315c9f01ffc3a6e070ca4137ca8536ded90
Vealex <mrvealex@gmail.com>
2023-08-09 14:33:28 +0700

    Created My Messenger app and add functionality

---
+++ MyMessenger/app/src/main/java/ru/vealex/mymessenger/CreateMessageActivity.java
@@ -0,0 +1,29 @@
+package ru.vealex.mymessenger;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+
+import android.view.View;
+
+import android.content.Intent;
+
+import android.widget.EditText;
+
+public class CreateMessageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    //Вызвать onSendMessage при щелчке по кнопке
+    public void onSendMessage(View view) {
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+        startActivity(intent);
+    }
+}
