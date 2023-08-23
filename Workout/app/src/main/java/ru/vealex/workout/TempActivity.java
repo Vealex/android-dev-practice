fbc2d22bc70d40dd809e37585917cbf9bde4eb8a
Vealex <mrvealex@gmail.com>
2023-08-23 17:50:46 +0700

    Added timer as fragment insertion

---
+++ Workout/app/src/main/java/ru/vealex/workout/TempActivity.java
@@ -0,0 +1,25 @@
+package ru.vealex.workout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentTransaction;
+
+
+public class TempActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temp);
+        if (savedInstanceState == null) {
+            StopwatchFragment stopwatch = new StopwatchFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.stopwatch_container, stopwatch);
+            ft.addToBackStack(null);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.commit();
+        }
+    }
+}
