4641e7cd268cbd578402c8e2834328fee8bd9eae
Vealex <mrvealex@gmail.com>
2023-08-26 21:46:26 +0700

    Add tabs, scroll collapsing, FAB, and snackbar

--- BitsandPizzas/app/src/main/java/ru/vealex/bitsandpizzas/MainActivity.java
+++ BitsandPizzas/app/src/main/java/ru/vealex/bitsandpizzas/MainActivity.java
@@ -1,9 +1,9 @@
 package ru.vealex.bitsandpizzas;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.appcompat.widget.Toolbar;
@@ -18,6 +18,16 @@ import androidx.appcompat.widget.ShareActionProvider;
 
 import androidx.core.view.MenuItemCompat;
 
+import androidx.fragment.app.Fragment;
+
+import androidx.fragment.app.FragmentManager;
+
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
 
     private ShareActionProvider shareActionProvider;
@@ -28,6 +38,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Связывание SectionPagerAdapter
+        SectionsPagerAdapter pagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(pagerAdapter);
+
+        // Связывание ViewPager с TabLayout
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(pager);
     }
 
     @Override
@@ -55,6 +74,41 @@ public class MainActivity extends AppCompatActivity {
             return true;
         } else {
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private class SectionsPagerAdapter extends FragmentPagerAdapter {
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0: return new TopFragment();
+                case 1: return new PizzaFragment();
+                case 2: return new PastaFragment();
+                case 3: return new StoresFragment();
+            }
+            return null;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0: return getResources().getString(R.string.home_tab);
+                case 1: return getResources().getString(R.string.pizza_tab);
+                case 2: return getResources().getString(R.string.pasta_tab);
+                case 3: return getResources().getString(R.string.store_tab);
+            }
+            return super.getPageTitle(position);
         }
     }
 }
--- BitsandPizzas/app/src/main/java/ru/vealex/bitsandpizzas/OrderActivity.java
+++ BitsandPizzas/app/src/main/java/ru/vealex/bitsandpizzas/OrderActivity.java
@@ -8,6 +8,12 @@ import androidx.appcompat.widget.Toolbar;
 
 import androidx.appcompat.app.ActionBar;
 
+import android.view.View;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import android.widget.Toast;
+
 public class OrderActivity extends AppCompatActivity {
 
     @Override
@@ -18,5 +24,18 @@ public class OrderActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void onClickDone(View view) {
+        CharSequence text = "Your order has been updated";
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.coordinator), text, Snackbar.LENGTH_SHORT);
+        snackbar.setAction("Undo", new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(OrderActivity.this, "Undone!", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+        snackbar.show();
     }
 }
